a6fbd66a214e00de538db8640e35111f6368bcb6
levanthang.0298 <levanthang.0298@gmail.com>
2023-12-18 11:15:56 +0700

    Code phien ban 1

---
+++ C01_BT01/C01_BT01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C01_BT01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Hello Worl");
+            Console.ReadLine();
+        }
+    }
+}
